44dcebb7d68eeb5ba52275a9f3ede33834646459
CodePlayer <ZK_Jakub@proton.me>
2022-12-10 17:47:33 +0100

    feat: increase the amount of information stored in the user's account
    Signed-off-by: CodePlayer <ZK_Jakub@proton.me>

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    zipcode = models.CharField(max_length=255, blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+User.userprofile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -2,23 +2,29 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from .forms import SignUpForm
+from .forms import SignUpForm, UserProfileForm
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+        userprofileform = UserProfileForm(request.POST)
 
-        if form.is_valid():
+        if form.is_valid() and userprofileform.is_valid():
             user = form.save()
+
+            userprofile = userprofileform.save(commit=False)
+            userprofile.user = user
+            userprofile.save()
 
             login(request, user)
 
             return redirect('frontpage')
     else:
         form = SignUpForm()
+        userprofileform = UserProfileForm()
 
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form, 'userprofileform': userprofileform})
 
 
 @login_required
