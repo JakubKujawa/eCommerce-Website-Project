2c6de9e73ba9f4aa8e2337934ff8d55c700a473b
CodePlayer <ZK_Jakub@proton.me>
2023-02-06 17:23:47 +0100

    :art: Change login redirect url
    Signed-off-by: CodePlayer <ZK_Jakub@proton.me>

--- eCommerceWebsite/settings.py
+++ eCommerceWebsite/settings.py
@@ -37,7 +37,7 @@ EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 # EMAIL_PORT = 1025
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'cart'
+LOGIN_REDIRECT_URL = 'myaccount'
 LOGOUT_REDIRECT_URL = 'frontpage'
 
 # Cart
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
